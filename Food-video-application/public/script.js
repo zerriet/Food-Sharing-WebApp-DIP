384c94c0b6a937eb35861e04f75a31521094a11f
zerriet <63580721+zerriet@users.noreply.github.com>
2020-12-08 02:23:05 +0800

    Update script.js

--- Food-video-application/public/script.js
+++ Food-video-application/public/script.js
@@ -1,9 +1,9 @@
 //showing video inside JS file, access video and audio
 
 //const { create } = require("domain");
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid'); //links to video grid in room.ejs
-const myVideo = document.createElement('video'); //creates video element
+const socket = io('/') //socket.io basically links script.js to server.js, whenever we reload our page we connect to our server, 
+const videoGrid = document.getElementById('video-grid'); //links to video grid in room.ejs, getting reference to video grid
+const myVideo = document.createElement('video'); //creates a new video element, our own camera stream links to this element
 myVideo.muted = true; //ensures you are not hearing your own voice
 
 
@@ -18,7 +18,7 @@ socket.on('new-user-connected', name => {
   })
 
 
-const iceServers = [
+const iceServers = [ //public TURN/STUN servers that creates connections for peer to peer communication
     { url: 'stun:stun.l.google.com:19302' },
     { url: "stun:eu-turn4.xirsys.com" },
     {
@@ -69,7 +69,7 @@ const iceServers = [
 ];
 
 
-var peer = new Peer({
+var peer = new Peer({ //creating a new peer on our side, peerJS wraps browser implementation for peer to peer communication
     path: '/peerjs',
     host: '/',
     port: '443',
@@ -101,40 +101,40 @@ port: '443' //3030 because local host 3030
 
 
 let myVideoStream //video variable
-navigator.mediaDevices.getUserMedia({
+navigator.mediaDevices.getUserMedia({//retrieval on audio and video stream
     video: true,
     audio: true
-}).then(stream => {
+}).then(stream => { //boardcasting our stream to other users
     myVideoStream = stream;
     console.log(stream.id);
     if (window.location.href.includes('?open=true')) {
         myVideoStream['local45454'] = true;
         stream['local45454'] = true;
     }
-    ApplyButtonsCheck();
-    addVideoStream(myVideo, stream);
+    ApplyButtonsCheck();//function that differentiates hosts and participants for donation system
+    addVideoStream(myVideo, stream); //adding our local video stream to actual UI so that it is visible
 
-    peer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
+    peer.on('call', call => {//telephone, allows user to retreive my stream while i receive his stream
+        call.answer(stream) //default method for peerJS library that sends other users our stream, similar to emitting value
+        const video = document.createElement('video')//creating stream element for other user
         call.on('stream', userVideoStream => {
             addVideoStream(video, userVideoStream) //add video stream from other user
         })
     })
 
-    socket.on('user-connected', (userId) => {
+    socket.on('user-connected', (userId) => {//references user id generated
 
-        console.log("user connected");
+        console.log("user connected");//tells our console when a user is connected
         setTimeout(function () {
             connecToNewUser(userId, stream);
-        }, 5000)
+        }, 5000)//time needs to be provided for video and audio stream to be given permission, hence the delay
 
         //connecToNewUser(userId, stream);
     });
 
-    let text = $('input')
+    let text = $('input')//creating a test element for our messages
 
-    $('html').keydown((e) => {
+    $('html').keydown((e) => {//shortcut for people to press enter when sending a message
         if (e.which == 13 && text.val().length !== 0) { //only enters key function when enter is pressed
             const data ={
                 type: 'chatMessage',
@@ -144,28 +144,28 @@ navigator.mediaDevices.getUserMedia({
             text.val('') //clears text field once enter is pressed
         }
     });
-    $('#submitReviewsBtn').click(function() {
-        $('#alertRef')[0].style.display = 'none';
+    $('#submitReviewsBtn').click(function() { //donation submitting, calls the function when the send button is clicked
+        $('#alertRef')[0].style.display = 'none';//alert for empty entries
         const pointsVal = $('#pointsLabels').val();
-        const donationsVal = $('#donationsLabels').val();
+        const donationsVal = $('#donationsLabels').val();//2 input values
         if (!pointsVal && !donationsVal) {
             $('#alertRef')[0].style.display = 'block';
             setTimeout(() => {
                 $('#alertRef')[0].style.display = 'none';
-            }, 3000);
+            }, 3000);//3 seconds before alert dissapears
             return;
         }
         $('#donationsLabels').val('');
         const data = {
           type: 'ReviewsSubmitted',
-          mData: {points: pointsVal, donations: donationsVal, userName: myName}
+          mData: {points: pointsVal, donations: donationsVal, userName: myName}//assigning donation and point values to 1 object
         }
         socket.emit('message', data);
         PresentToaster('Yay! Gift successfully sent. Thank you for your lovely gift!');
         $('#exampleModal').modal('toggle');
     });
-    $('#openModalClicked').click(function() {
-        $('#pointsLabels').val('');
+    $('#openModalClicked').click(function() {//open modal for donations
+        $('#pointsLabels').val('');//references for donation, clears the field from previously once opened
         $('#donationsLabels').val('');
     });
 
@@ -176,32 +176,32 @@ navigator.mediaDevices.getUserMedia({
             $('.messages').append(`<li class = "message"><b>${data.mData.userName}</b><br/>${data.mData.message}</li>`) // when every message, class message will have user and the message the user sends
             scrollToBottom()
         }
-        if (data && data.type === 'ReviewsSubmitted') {
+        if (data && data.type === 'ReviewsSubmitted') {//receivers or hosts end
             const { points, donations } = data.mData;
-            const newPoints = +$('#PointsCount').html() + (+points);
+            const newPoints = +$('#PointsCount').html() + (+points);//counter adding our object data
             $('#PointsCount').html(newPoints);
             const newDonations = (+$('#DonationsCount').html()) + (+donations);
             $('#DonationsCount').html('');
-            $('#DonationsCount').html(newDonations);
-            if (myVideoStream['local45454']) {
+            $('#DonationsCount').html(newDonations); //clears field before adding new object
+            if (myVideoStream['local45454']) { //local45454 references hosts only
                 PresentToaster('New points or donations received by ' + data.mData.userName, true);
             }
         }
-        if (data && data.type === 'areYouTutor') {
+        if (data && data.type === 'areYouTutor') { //differentiates between hosts and participants 
             if (window.location.href.includes('?open=true')) {
                 const data = {
-                    type: 'yesIamTutor',
+                    type: 'yesIamTutor',//references for hosts only
                     mData: myVideoStream.id
                 }
                 socket.emit('message', data);
             }
         }
         if (data && data.type === 'yesIamTutor') {
-            var video = document.getElementById(data.mData);
-            video.className = 'videoBig';
+            var video = document.getElementById(data.mData);//if i am a host, app retrieves and parses my video data
+            video.className = 'videoBig'; //assigning video big class
             var grid = $('#video-grid').children();
-            const tablerow = $('#' + video.id);
-            tablerow.prependTo('#video-grid')[0];
+            const tablerow = $('#' + video.id);//groups videos together
+            tablerow.prependTo('#video-grid')[0]; //prepend means attaching to the first element
             // $('#element1').insertAfter($('#element2'));
             // $(`#${video.id}`).insertAfter(`#${grid[0].id}`);
             // $(`#${video.id}`).swap(`#${grid[0].id}`);
@@ -215,7 +215,7 @@ navigator.mediaDevices.getUserMedia({
 //promise is an event in the future that will be resolved or rejected
 
 
-peer.on('open', id => {
+peer.on('open', id => {//tells a socket that i am connecting to this persons id
     //console.log(id);
     socket.emit('join-room', ROOM_ID, id);
 });
@@ -226,7 +226,7 @@ peer.on('open', id => {
     connecToNewUser(userId, stream);
 });*/
 
-const connecToNewUser = (userId, stream) => {
+const connecToNewUser = (userId, stream) => {//for participants only
     //console.log(userId); //informs when a new user has connected
     const call = peer.call(userId, stream) //call user id, send him my stream, and I will recieve his stream
     const video = document.createElement('video')
@@ -236,30 +236,30 @@ const connecToNewUser = (userId, stream) => {
             type: 'areYouTutor',
             mData: ''
         }
-        socket.emit('message', data);
+        socket.emit('message', data);//function for transferring any form of data
     })
 };// other persons stream
 
-function PresentToaster(message, info) {
+function PresentToaster(message, info) { //applies toaster, generic functionality
     // $('button').click(function () {
     //     $('.toast').stop().fadeIn(400).delay(3000).fadeOut(500); //fade out after 3 seconds
     // });
     
     // $('.toast').stop().fadeIn(400).delay(3000).fadeOut(500);
-    var priority = info ? 'info' : 'success';
+    var priority = info ? 'info' : 'success';//color of toaster
     var title    = 'Success';
-    $.toaster({ priority : priority, title : title, message : message });
+    $.toaster({ priority : priority, title : title, message : message });//providing values to our toaster
 }
 
-function ApplyButtonsCheck() {
+function ApplyButtonsCheck() {//differentiate for donation functionality
     if (myVideoStream['local45454']) {
-        $('#pCheck')[0].style.display = 'block';
+        $('#pCheck')[0].style.display = 'block'; //donation and points for hosts
         $('#dCheck')[0].style.display = 'block';
     } else {
-        $('#openModalClicked')[0].style.display = 'block';
+        $('#openModalClicked')[0].style.display = 'block'; //open modal for non hosts
     }
 }
-function addVideoStream(video, stream) {
+function addVideoStream(video, stream) {//my screen
     video.srcObject = stream;
     video.id = stream.id;
     video.addEventListener('loadedmetadata', () => {
@@ -302,7 +302,7 @@ const scrollToBottom = () => { //function that allows chat to autoscroll downwar
 //Mute our video
 const muteUnmute = () => {
     console.log(myVideoStream)
-    const enabled = myVideoStream.getAudioTracks()[0].enabled; //get current enabled audo tracj
+    const enabled = myVideoStream.getAudioTracks()[0].enabled; //get current enabled audo track
     if (enabled) {
         myVideoStream.getAudioTracks()[0].enabled = false; //set audio to false
         setUnmuteButton(); //button icon changes depending on mute or unmute
@@ -313,19 +313,19 @@ const muteUnmute = () => {
 }
 const CopyLinkToMeeting = () => {
     let nUrl = '';
-    nUrl = window.location.href.split('?')[0] + '?open=false';
+    nUrl = window.location.href.split('?')[0] + '?open=false'; //whenever you press copy, it will always append = false
     const textArea = document.createElement('textarea');
     textArea.style.visibility = 'none';
     textArea.value = nUrl;
     document.body.appendChild(textArea);
-    textArea.select();
+    textArea.select(); //taking reference to our url
     textArea.setSelectionRange(0, 99999);
-    document.execCommand('copy');
-    textArea.remove();
+    document.execCommand('copy'); //if you want to copy anything, this is the default comand
+    textArea.remove(); //once we copy, remove dummy element
     document.getElementById('copyBtn').innerHTML = 'Copied';
     setTimeout(() => {
         document.getElementById('copyBtn').innerHTML = 'Copy';
-    }, 4000);
+    }, 4000); //whenever we press copy, button changes to copied before reverting back
 }
 
 const setMuteButton = () => {
